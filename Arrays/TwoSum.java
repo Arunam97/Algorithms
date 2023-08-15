38f6059e12b4d45fd57a44b8d1889fe5e942c43d
Arunam Gupta <arunamgupta97@gmail.com>
2023-08-15 14:10:09 -0400

    Added package arrays

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,22 @@
+package Arrays;
+import java.util.*;
+/*
+ * https://leetcode.com/problems/two-sum/
+ * Given an array of integers 'nums' and an integer 'target', return indices of the two numbers such that they add up to target
+ */
+class TwoSum {
+    public int[] TwoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int size = nums.length;
+        for(int i = 0; i < size; i++) 
+        {
+            int complement = target - nums[i]; // Find the complement of the current element
+            if(map.containsKey(complement)) // Check if that complement exists in our map
+            {
+                return new int[] {map.get(complement), i}; // If complement exists, return both current index and complement's index
+            }
+            map.put(nums[i], i); // If complement was not in the map, then enter the current number and its index as key-value pair
+        }
+        return new int[] {}; // No solution found
+    }
+}
